b126bedd526d267424050eef87d0576bae49b5db
clementGilardy <gilardy.clement@gmail.com>
2016-03-02 19:22:01 +0100

    change project

--- app/js/services.js
+++ app/js/services.js
@@ -3,6 +3,6 @@ var champServices = angular.module('champServices', ['ngResource']);
 champServices.factory('Champ', ['$resource',
     function($resource){
         return $resource('https://euw.api.pvp.net/api/lol/euw/v1.2/champion?api_key=36b93c1a-a7e5-46a6-a7fd-59ff6710080c', {}, {
-            query: {method:'GET', isArray:true}
+            query: {method:'GET'}
         });
     }]);
